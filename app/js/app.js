e12c3fcd271cbfe93bdb447745d1fa289ca10033
lolochka <ooshkoda@gmail.com>
2015-07-16 00:29:21 +0300

    Add nesting, showing and hiding form

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,82 @@
 'use strict';
 
 /* App Module */
-var crewApp = angular.module('crewApp', ['ngRoute', 'crewControllers', 'crewServices']);
+var crewApp = angular.module('crewApp', ['ngRoute', 'crewControllers', 'crewServices', 'route-segment', 'view-segment']);
 
-crewApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/employees', {
+crewApp.config(['$routeSegmentProvider', '$routeProvider', 
+  function($routeSegmentProvider, $routeProvider) {
+    
+    $routeSegmentProvider
+      .when('/employees',                  'employees')
+      .when('/employees/:employeeId',      'employees.employeeDetail')
+      .when('/about',                      'about')
+      .when('/about/first',                'about.s1')
+      .when('/about/second',               'about.s2')
+      .when('/about/third',                'about.s3')
+      .when('/faq',                        'faq')
+      .when('/contact',                    'contact')
+
+      .segment('employees', {
         templateUrl: 'partials/employees.html',
         controller: 'EmployeeListCtrl'
-      }).
-      when('/employees/:employeeId', {
-        templateUrl: 'partials/employee-detail.html',
-        controller: 'EmployeeListCtrl'
-      }).
+      })
+
+      .within()
+        .segment('home', {
+          default: true,
+          templateUrl: 'partials/employees/home.html'
+        })
+
+        .segment('employeeDetail', {
+          templateUrl: 'partials/employees/employee-detail.html',
+          dependencies:['employeeId']
+        })
+
+        .up()
+
+      .segment('about', {
+        templateUrl: 'partials/about.html',
+        controller: 'AboutCtrl'
+      })
+
+      .within()
+        .segment('s1', {
+          default: true,
+          templateUrl: 'partials/about/first.html'
+        })
+
+        .segment('s2', {
+          templateUrl: 'partials/about/second.html'
+        })
+
+        .segment('s3', {
+          templateUrl: 'partials/about/third.html'
+        })
+
+        .up()
+
+      .segment('faq', {
+        templateUrl: 'partials/faq.html',
+        controller: 'FaqCtrl'
+      })
+
+      .segment('contact', {
+        templateUrl: 'partials/contact.html'
+      })
+
+    $routeProvider.otherwise({redirectTo: '/employees'});
+  //   $routeProvider.
+  //     when('/employees', {
+  //       templateUrl: 'partials/employees.html',
+  //       controller: 'EmployeeListCtrl'
+  //     }).
+  //     when('/employees/:employeeId', {
+  //       templateUrl: 'partials/employee-detail.html',
+  //       controller: 'EmployeeListCtrl'
+  //     }).
       
-      otherwise({
-        redirectTo: '/employees'
-      });
+  //     otherwise({
+  //       redirectTo: '/employees'
+  //     });
   }
 ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,97 +4,128 @@
 
 var crewControllers = angular.module('crewControllers', []);
 
-crewControllers.controller('EmployeeListCtrl', ['$scope', '$routeParams', 'Cache', 
-  function($scope, $routeParams, Cache) {
-    $scope.employees = Cache.getItems();
-    $scope.getPhoto = function(obj) {
-      if (obj.photoUrl != 0) {
-         return obj.photoUrl;
-      } else {
-        return 'img/user.png';
-      }
-    };
-    $scope.departments = [
-      "web", "mobile", "testing", "sales"
-    ];
-    $scope.emplRoute = $routeParams.employeeId;
+crewControllers.controller('EmployeeListCtrl', ['$scope', '$routeParams', 'Cache', '$routeSegment', function($scope, $routeParams, Cache, $routeSegment) {
+  $scope.employees = Cache.getItems();
+  $scope.$routeSegment = $routeSegment;
 
-    $scope.getEmployee = function(param) {
-      var arr = $scope.employees;
+  $scope.departments = [
+    "web", "mobile", "testing", "sales"
+  ];
+
+  $scope.$routeParams = $routeParams.employeeId;
+
+  $scope.getEmployee = function(param) {
+    var arr = $scope.employees;
+    if (arr != undefined) {
       for ( var i = 0; i < arr.length; i++ ) {
-        if ( arr[i].id = param) {
-          console.log(arr[i]);
-          return arr[i];
+        if ( arr[i].employeeId = param) {
+        console.log(arr[i]);
+        return arr[i];
         }
       }
     }
+  };
 
-    $scope.thisEmployee = $scope.getEmployee($scope.emplRoute);
-    console.log();
-
-    $scope.getPhoto = function(obj) {
-      if (obj.photoUrl != 0) {
-        return obj.photoUrl;
-      } else {
-        return 'img/user.png';
-      }
-
-    };
-
-    $scope.deleteEmployee = function(param) {
-      Cache.removeItem(param);
-
-    };
-
-    $scope.getExperience = function(obj) {
-      if (obj.year != 0 || obj.month != 0 ) {
-        var month = obj.month;
-        var year = obj.year;
-        var date = new Date();
-        var nowMonth = date.getMonth();
-        var nowYear = date.getFullYear();
-        var diffMonth;////need compile
-        var diffYear;
-        month == 0 ? diffMonth = 0 : diffMonth = nowMonth - month;
-        year == 0 ? diffYear = 0 : diffYear = nowYear - year;
-        if (diffMonth > 0) {
-          if (diffYear > 0) {
-            return diffYear + ' year(s) and ' + diffMonth + ' month(s)';
-          } else if (diffYear == 0) {
-            return diffMonth + ' month(s)';
-          } else {
-            return 'Have not started yet';
-          }
-        } else if (diffMonth == 0) {
-          if (diffYear > 0) {
-            return diffYear + ' year(s)';
-          } else if (diffYear == 0) {
-            return 'Do not have any experience';
-          } else {
-              return 'Have not started yet';
-          }
-        } else {
-          diffMonth = diffMonth + 12;
-          diffYear = diffYear - 1;
-          if (diffYear > 0) {
-            return diffYear + ' year(s) and ' + diffMonth + ' month(s)';
-          } else if (diffYear === 0) {
-            return diffMonth + ' month(es)';
-          } else {
-            return 'Have not started yet';
-          }
-        }//need compile
-      } else {
-        return false;
-      }
+  $scope.length = function(obj) {
+    if (obj != 0) {
+      return obj.length;
+    } else { 
+      return 0;
     }
+  };
 
-  }]);
+  console.log($scope.$routeSegment);
+  console.log($scope.$routeParams);
+
+  $scope.thisEmployee = $scope.getEmployee($scope.$routeParams);
 
 
-      
+  $scope.getPhoto = function(obj) {
+    if (obj.photoUrl != 0) {
+      return obj.photoUrl;
+    } else {
+      return 'img/user.png';
+    }
+  };
 
-  //     
-  //   });
-    
-  // }]);//сюда можно подключить переменной дата из локал сторедж
+  $scope.getExperience = function(obj) {
+    if (obj.year != 0 || obj.month != 0 ) {
+      var month = obj.month;
+      var year = obj.year;
+      var date = new Date();
+      var nowMonth = date.getMonth();
+      var nowYear = date.getFullYear();
+      var diffMonth;////need compile
+      var diffYear;
+      month == 0 ? diffMonth = 0 : diffMonth = nowMonth - month;
+      year == 0 ? diffYear = 0 : diffYear = nowYear - year;
+      if (diffMonth > 0) {
+        if (diffYear > 0) {
+          return diffYear + ' year(s) and ' + diffMonth + ' month(s)';
+        } else if (diffYear == 0) {
+          return diffMonth + ' month(s)';
+        } else {
+          return 'Have not started yet';
+        }
+      } else if (diffMonth == 0) {
+        if (diffYear > 0) {
+          return diffYear + ' year(s)';
+        } else if (diffYear == 0) {
+          return 'Do not have any experience';
+        } else {
+          return 'Have not started yet';
+        }
+      } else {
+        diffMonth = diffMonth + 12;
+        diffYear = diffYear - 1;
+        if (diffYear > 0) {
+          return diffYear + ' year(s) and ' + diffMonth + ' month(s)';
+        } else if (diffYear === 0) {
+          return diffMonth + ' month(es)';
+        } else {
+          return 'Have not started yet';
+        }
+      }//need compile
+    } else {
+      return false;
+    }
+  }
+
+  $scope.deleteEmployee = function(param) {
+    Cache.removeItem(param);
+  };
+
+}]);
+
+
+
+crewControllers.controller('AboutCtrl', ['$scope', function($scope) {
+  $scope.about = {
+    'first' : 'blablabka',
+    'second' : 'Lolooooodlllld',
+    'third' : 'Hdlkjaflhflaffja'
+  }
+}]);
+//сюда можно подключить переменной дата из локал сторедж
+
+
+crewControllers.controller('FaqCtrl', ['$scope', function($scope) {
+  $scope.about = [
+  {
+    'question' : 'blablabka',
+    'answer' : 'Lolooooodlllld'
+  },
+  {
+    'question' : 'blablabka',
+    'answer' : 'Lolooooodlllld'
+  },
+  {
+    'question' : 'blablabka',
+    'answer' : 'Lolooooodlllld'
+  },
+  {
+    'question' : 'blablabka',
+    'answer' : 'Lolooooodlllld'
+  }
+  ]
+}]);
